7574502fdf0e166909d0eff0fa5af5efd08f63c3
OpenShift guest <magicmanfr42@hotmail.com>
2013-01-30 07:15:11 -0500

    fromcl9

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,7 +9,7 @@ class MoviesController < ApplicationController
   def index
     @all_ratings = Movie.aratings()
     if params[:sort]==nil
-        @movies = Movie.where("rating = ?" , params[ratings].keys)
+        @movies = Movie.where("rating = ?" , params[:ratings].keys)
     elsif params[:sort]=="byname"
         @movies = Movie.find(:all, :order => "title")
     elsif params[:sort]=="bydate"
