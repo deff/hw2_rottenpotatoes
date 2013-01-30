9f83671dde8f699c733675d159cc8de363fb8095
OpenShift guest <magicmanfr42@hotmail.com>
2013-01-30 05:04:31 -0500

    fromcl9

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-    :before_filter :loadr , :only => :index 
+    before_filter :loadr , :only => :index 
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
